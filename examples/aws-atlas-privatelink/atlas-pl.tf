70f87157467b43dea36ccf03f7ceaf4e1632784f
Robbert Kauffman <robbertkauffman@gmail.com>
2023-02-17 14:50:03 -0500

    improved descriptions and names

--- examples/aws-atlas-privatelink/atlas-pl.tf
+++ examples/aws-atlas-privatelink/atlas-pl.tf
@@ -1,20 +1,20 @@
-resource "mongodbatlas_privatelink_endpoint" "atlaspl" {
-  project_id    = var.atlasprojectid
+resource "mongodbatlas_privatelink_endpoint" "atlas_private_endpoint" {
+  project_id    = var.atlas_project_id
   provider_name = "AWS"
   region        = var.aws_region
 }
 
-resource "aws_vpc_endpoint" "ptfe_service" {
+resource "aws_vpc_endpoint" "vpc_endpoint" {
   vpc_id             = aws_vpc.primary.id
-  service_name       = mongodbatlas_privatelink_endpoint.atlaspl.endpoint_service_name
+  service_name       = mongodbatlas_privatelink_endpoint.atlas_private_endpoint.endpoint_service_name
   vpc_endpoint_type  = "Interface"
   subnet_ids         = [aws_subnet.primary-az1.id, aws_subnet.primary-az2.id]
   security_group_ids = [aws_security_group.primary_default.id]
 }
 
-resource "mongodbatlas_privatelink_endpoint_service" "atlaseplink" {
-  project_id          = mongodbatlas_privatelink_endpoint.atlaspl.project_id
-  endpoint_service_id = aws_vpc_endpoint.ptfe_service.id
-  private_link_id     = mongodbatlas_privatelink_endpoint.atlaspl.id
+resource "mongodbatlas_privatelink_endpoint_service" "atlas_endpoint_service" {
+  project_id          = mongodbatlas_privatelink_endpoint.atlas_private_endpoint.project_id
+  endpoint_service_id = aws_vpc_endpoint.vpc_endpoint.id
+  private_link_id     = mongodbatlas_privatelink_endpoint.atlas_private_endpoint.id
   provider_name       = "AWS"
 }
--- examples/aws-atlas-privatelink/variables.tf
+++ examples/aws-atlas-privatelink/variables.tf
@@ -1,11 +1,14 @@
-variable "atlasprojectid" {
-  description = "Atlas project ID"
+variable "atlas_project_id" {
+  description = "Atlas project ID (e.g. 5beae24579358e0ae95492af)"
+  type        = string
 }
+
 variable "atlas_region" {
-  default     = "US_EAST_1"
-  description = "Atlas Region"
+  description = "Atlas region (e.g. US_EAST_2)"
+  type        = string
 }
+
 variable "aws_region" {
-  default     = "us-east-1"
-  description = "AWS Region"
+  description = "AWS region (e.g. us-east-2)"
+  type        = string
 }
