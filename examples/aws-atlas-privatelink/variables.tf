964ebf8b75aa022da7363448cfd0dfeaf6864770
Robbert Kauffman <robbertkauffman@gmail.com>
2023-02-17 14:49:19 -0500

    move Atlas and AWS credentials out of variables

--- examples/aws-atlas-privatelink/variables.tf
+++ examples/aws-atlas-privatelink/variables.tf
@@ -1,17 +1,5 @@
-variable "public_key" {
-  description = "The public API key for MongoDB Atlas"
-}
-variable "private_key" {
-  description = "The private API key for MongoDB Atlas"
-}
 variable "atlasprojectid" {
   description = "Atlas project ID"
-}
-variable "access_key" {
-  description = "The access key for AWS Account"
-}
-variable "secret_key" {
-  description = "The secret key for AWS Account"
 }
 variable "atlas_region" {
   default     = "US_EAST_1"
