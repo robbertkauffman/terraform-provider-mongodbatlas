1be96e3ba70bbbb41718e0ce0a0212e2a1fcf1e7
Robbert Kauffman <robbertkauffman@gmail.com>
2023-02-17 14:50:27 -0500

    added EC2 jumphost for verifying connection to PE

---
+++ examples/aws-atlas-privatelink/aws-ec2.tf
@@ -0,0 +1,36 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
+}
+
+resource "aws_network_interface" "primary_eni" {
+  subnet_id       = aws_subnet.primary-az1.id
+  security_groups = [aws_security_group.primary_default.id]
+}
+
+# Create jumphost for verifying connectivity to cluster via PE
+resource "aws_instance" "ec2_jumphost" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.primary_eni.id
+    device_index         = 0
+  }
+}
