39cdb20f268037a88680a93629376ea34a5f77fc
Robbert Kauffman <robbertkauffman@gmail.com>
2023-03-03 11:37:55 -0500

    removed any unnecessary config from cluster

--- examples/aws-atlas-privatelink/atlas-cluster.tf
+++ examples/aws-atlas-privatelink/atlas-cluster.tf
@@ -2,9 +2,6 @@
 resource "mongodbatlas_cluster" "cluster-atlas" {
   project_id                   = var.atlas_project_id
   name                         = "cluster-atlas"
-  cloud_backup                 = true
-  auto_scaling_disk_gb_enabled = true
-  mongo_db_major_version       = "4.2"
   cluster_type                 = "REPLICASET"
   replication_specs {
     num_shards = 1
@@ -17,7 +14,6 @@ resource "mongodbatlas_cluster" "cluster-atlas" {
   }
   # Provider settings
   provider_name               = "AWS"
-  disk_size_gb                = 10
   provider_instance_size_name = "M10"
 }
 
